585795c9564ff53d395c81c6de3be665e1b68d9f
Linda Quelex <lindamadelin@gmail.com>
2022-06-23 00:06:43 -0600

    LEX DEF TOKENS Y RESERVADAS

---
+++ PROYECTO2/ENTRADAS/p2.sc
@@ -0,0 +1,11 @@
+INICIO
+56*9-5/2552
+FINm true false _identif
+string
+15.5
+int
+double
+string
+char
+boolean
+$
--- PROYECTO2/analizador_ply.py
+++ PROYECTO2/analizador_ply.py
@@ -6,10 +6,23 @@ def getColumn(t):
   line_start = INPUT.rfind('\n', 0, t.lexpos) + 1
   return (t.lexpos-line_start)+1
 
-# Tokens
+reserved = {
+  'true': 'tk_boolean_true',
+  'false':'tk_boolean_false',
+  'int':  't_tk_tipo_int',
+  'double': 't_tk_tipo_double',
+  'string': 't_tk_tipo_string',
+  'char': 't_tk_tipo_char',
+  'boolean': 't_tk_tipo_boolean',
+  'else': 't_tk_condicional_else',
+  'while': 't_tk_iterativo_while',
+  'do': 't_tk_iterativo_do',
+  'void':'t_tk_reservda_void',
+  'return':'t_tk_reservada_return',
+}
+
 tokens = (
   #LEXEMAS DEL PROYECTO, AUN FALTA AGREGAR LO NUEVO
-  'tk_identificador',
   'tk_comentario_var_filas',
   'tk_tipo_int',
   'tk_tipo_double',
@@ -40,8 +53,6 @@ tokens = (
   'tk_reservda_void',
   'tk_reservada_return',
   'tk_igualacion',
-  'tk_boolean_true',
-  'tk_boolean_false',
   'tk_asignacion', 
   'tk_comentario_simple',
   'tk_division',
@@ -49,8 +60,8 @@ tokens = (
   'tk_dato_tipo_Int',
   'tk_dato_char',
   'tk_dato_string',
-  
-)
+  'tk_identificador',
+) + tuple(reserved.values())
 
 t_tk_tipo_int=r'int'
 t_tk_tipo_double=r'double'
@@ -86,7 +97,6 @@ t_tk_boolean_false=r'false'
 t_tk_asignacion=r'='
 t_tk_division=r'/'
 
-
 # Lexemas ignorados
 t_ignore = ' \t\r\n'
 
@@ -96,9 +106,8 @@ t_ignore = ' \t\r\n'
     - value: valor del lexema
     - type: nombre del token
 """
-
-def t_tk_identificador(t):
-  r'[a-zA-Z_][a-zA-Z_0-9]*'
+def t_tk_dato_double(t):
+  r'\d+.\d\d*'
   if t.value in tokens: 
     t.type = t.value
   return t
@@ -109,21 +118,35 @@ def t_tk_dato_tipo_Int(t):
     t.type = t.value
   return t
 
-# def t_tk_comentario_var_filas(t): 
-#   pass
+def t_tk_identificador(t):
+  r'[a-zA-Z_][a-zA-Z_0-9]*'
+  if t.value in reserved.keys():
+    t.type = reserved[t.value]
+  return t
 
-# def t_tk_comentario_simple(t):
-#   pass 
+def t_tk_comentario_var_filas(t): 
+  r'\/\*(.*\n*)*\*\/'
+  if t.value in tokens: 
+    t.type = t.value
+  return t
 
-# def t_tk_dato_double(t):
-#   pass
+def t_tk_comentario_simple(t):
+  r'\/\/.*'
+  if t.value in tokens: 
+    t.type = t.value
+  return t
 
-# def t_tk_dato_char(t):
-#   pass
+def t_tk_dato_char(t):
+  r'\'.{1}\''
+  if t.value in tokens: 
+    t.type = t.value
+  return t
 
-# def  t_tk_dato_string(t):
-#   pass 
-
+def  t_tk_dato_string(t):
+  r'\".*\"'   # no puede aceptar " dentro de las comillas del string
+  if t.value in tokens: 
+    t.type = t.value
+  return t
 
 def t_newline(t):
   r'\n+'
@@ -139,87 +162,26 @@ INPUT = r'''
 INICIO
 56*9-5/2552
 FINm true false _identif
+string
+15.5
+int
+double
+string
+char
+boolean
+TRUE
+//com afs
+salir
+"errorchar"
+/* coment
+var 
+lineas*/
+'o'
+'h'
 $
 '''
 
-lexer.input(INPUT)
+lexer.input(INPUT.lower())
 
 for tok in lexer:
   print(tok)
-
-
-
-  #-------------------------------------------------
-
-
-# def getColumn(t):
-#   line_start = INPUT.rfind('\n', 0, t.lexpos) + 1
-#   return (t.lexpos-line_start)+1
-
-# # Tokens
-
-# reserved = (
-#   'reservada_inicio',
-#   'reservada_fin'
-# )
-
-# tokens = reserved + (
-#   'operador_suma',
-#   'operador_resata',
-#   'operador_multiplicacion',
-#   'operador_division',
-#   'operador_igualacion',
-#   'operador_resto',
-#   'numero',
-#   'id',
-# )
-
-# t_reservada_inicio = r'INICIO'
-# t_reservada_fin = r'FIN'
-# t_operador_suma = r'\+'
-# t_operador_resata = r'-'
-# t_operador_multiplicacion = r'\*'
-# t_operador_division = r'/'
-# t_operador_resto = r'%'
-# t_numero = r'\d+'
-
-# # Lexemas ignorados
-# t_ignore = ' \t\r\n'
-
-
-# """
-#   t:
-#     - lineno: numero de linea
-#     - value: valor del lexema
-#     - type: nombre del token
-# """
-
-# def t_id(t): # t_id
-#   r'[a-zA-Z_][a-zA-Z_0-9]*'
-
-#   if t.value in reserved: t.type = t.value
-
-#   return t
-
-# def t_newline(t):
-#   r'\n+'
-#   t.lexer.lineno+=len(t.value)
-
-# def t_error(t):
-#   print(t.lineno, getColumn(t), f"No se pudo reconocer el lexema '{t.value}'")
-#   t.lexer.skip(1)
-
-# lexer = lex()
-
-# INPUT = r'''
-# INICIO
-# 56*9-5/2552
-# FIN
-# prueba
-
-# '''
-
-# lexer.input(INPUT)
-
-# for tok in lexer:
-#   print(tok)
